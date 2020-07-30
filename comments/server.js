abc2a565b0be3441ec4229a633894dae707e0a24
Patrick Furtak <patrick@patrickfurtak.com>
2020-07-30 19:36:44 -0400

    Fixed cors block during front end to back end data transfer

--- comments/server.js
+++ comments/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 
 const commentsByPostId = {};
 
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
