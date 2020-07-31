9f9aee6df26ce7b8704633a2bf32dbe23edde26d
Patrick Furtak <patrick@patrickfurtak.com>
2020-07-31 19:14:37 -0400

    Fixed data structure object on comment post req

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -18,7 +18,7 @@ export default ({ postId }) => {
 
   const renderedComments = comments.map((comment) => {
     console.log(comment);
-    return <li key={comment.id}>{comment.content.content}</li>;
+    return <li key={comment.id}>{comment.content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
