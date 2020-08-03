336be675017e702f1d6fca03b1d9620b88a510bf
Patrick Furtak <patrick@patrickfurtak.com>
2020-08-03 00:30:44 -0400

    Started writing logic for query service. Fixed an infinite request bug in the eventbus. Commented event bus dispatch locations for better clarity

--- event-bus/server.js
+++ event-bus/server.js
@@ -2,15 +2,19 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-
 app.use(express.json({ extended: false }));
 
 app.post('/events', (req, res) => {
   const event = req.body;
 
+  //post service
   axios.post('http://localhost:4000/events', event);
+
+  //coment service
   axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4005/events', event);
+
+  //query service
+  axios.post('http://localhost:4002/events', event);
 
   res.send({ status: 'OK' });
 });
