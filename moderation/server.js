d475e614b19d0fca6443bb1daa74a81ff2a08785
Patrick Furtak <patrick@patrickfurtak.com>
2020-09-09 17:19:39 -0400

    Linked event bus to moderation service, setup moderation service to alter status based on if the comment contains the word orange

--- moderation/server.js
+++ moderation/server.js
@@ -4,7 +4,24 @@ const axios = require('axios');
 const app = express();
 app.use(express.json({ extended: false }));
 
-app.post('/events', (req, res) => {});
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+  res.send({});
+});
 
 app.listen(4003, () => {
   console.log('Moderation service listening on port 4003.');
